912ab1970cded864cd84e3a29e9d258592245b90
Aryan Tiwari <aryantiwarimorena@gmail.com>
2025-07-14 17:21:02 +0530

    Add Project Files

---
+++ models/listing.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const review = require("./review");
+const Schema = mongoose.Schema;
+const Review = require("./review.js");
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    url: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1737562963380-3a7e45c0bf31?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDQ0fDZzTVZqVExTa2VRfHxlbnwwfHx8fHw%3D",
+      set: (v) =>
+        v === ""
+          ? "https://images.unsplash.com/photo-1737562963380-3a7e45c0bf31?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDQ0fDZzTVZqVExTa2VRfHxlbnwwfHx8fHw%3D"
+          : v,
+    },
+  },
+  price: Number,
+  location: String,
+  country: String,
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
